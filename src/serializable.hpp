b33dcf2de169848698cbbc0d4b50da4a00920fd1
Jonathan Fung <jonathanfung2000@gmail.com>
2024-10-30 15:53:40 -0700

    serialize std::unique_ptr<T>

--- src/serializable.hpp
+++ src/serializable.hpp
@@ -7,3 +7,15 @@ template<typename T>
 concept Serializable = requires(const T& a, json& j) {
   { to_json(j, a) } -> std::same_as<void>;
 };
+
+
+template<typename T>
+void
+to_json(json& j, const std::unique_ptr<T>& unq)
+{
+  if (unq) {
+    j = *unq;
+  } else {
+    j = nullptr;
+  }
+}
