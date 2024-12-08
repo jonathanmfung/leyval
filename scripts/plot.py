bbb0f7616a7bdd7cdddb44af73c0d208b9004c6a
Jonathan Fung <jonathanfung2000@gmail.com>
2024-12-08 12:02:23 -0800

    scripts: start porting to class for data processing

--- scripts/plot.py
+++ scripts/plot.py
@@ -11,33 +11,49 @@ IMG_DIR = "img/"
 FIGSIZE = (8, 4.5)
 DPI = 200
 
-def read_agents(agents):
-    return pd.json_normalize(agents)
+class LeyvalPlotter:
+    def __init__(self, data_file):
+        self.data_file = data_file
+        self._agents_raw = []
+        self._book_raw = []
+        self.agents_clean: pd.DataFrame
+        self.book_clean: pd.DataFrame
 
-def read_book(book):
-    bids = pd.Series([x[1] for x in book['bid_counts']],
-                     index = [x[0] for x in book['bid_counts']],
-                     name='bids')
-    asks = pd.Series([x[1] for x in book['ask_counts']],
-                     index = [x[0] for x in book['ask_counts']],
-                     name='asks')
-    return pd.concat([bids, asks], keys=['bids', 'asks'])
+    @staticmethod
+    def _read_agents(agents_json):
+        return pd.json_normalize(agents_json)
 
-with open(DATA_FILE, 'r') as f:
-    raw_json = json.load(f)
+    @staticmethod
+    def _read_book(book_json):
+        bids = pd.Series([x[1] for x in book_json['bid_counts']],
+                         index = [x[0] for x in book_json['bid_counts']],
+                         name='bids')
+        asks = pd.Series([x[1] for x in book_json['ask_counts']],
+                         index = [x[0] for x in book_json['ask_counts']],
+                         name='asks')
+        return pd.concat([bids, asks], keys=['bids', 'asks'])
 
-data_agents = []
-data_book = []
-for run_tick in raw_json:
-    data_agents.append(read_agents(run_tick['agents']))
-    data_book.append(read_book(run_tick['order_book']))
+    def read_raw(self):
+        with open(self.data_file, 'r') as f:
+            raw_json = json.load(f)
 
-## Clean
-clean_agents = pd.concat(data_agents, keys = range(len(data_agents))).drop(columns='id')
-clean_agents.index = clean_agents.index.set_names(['time', 'id'])
+        for run_tick in raw_json:
+            self._agents_raw.append(self._read_agents(run_tick['agents']))
+            self._book_raw.append(self._read_book(run_tick['order_book']))
 
-clean_book = pd.concat(data_book, axis=1).T.fillna(0).astype("Int64")
-clean_book.index = clean_book.index.set_names('time')
+    def clean_data(self):
+        self.agents_clean = pd.concat(self._agents_raw,
+                                      keys = range(len(self._agents_raw)))\
+                              .drop(columns='id')
+        self.agents_clean.index = self.agents_clean.index.set_names(['time', 'id'])
+
+        self.book_clean = pd.concat(self._book_raw, axis=1)\
+                            .T.fillna(0).astype("Int64")
+        self.book_clean.index = self.book_clean.index.set_names('time')
+
+lp = LeyvalPlotter(DATA_FILE)
+lp.read_raw()
+lp.clean_data()
 
 ## plot agents
 fig, ax = plt.subplots(figsize=FIGSIZE, dpi=DPI, layout='constrained')
@@ -86,28 +102,28 @@ plt.savefig(IMG_DIR + "agents.png")
 ## Book FuncAnimation
 fig, ax = plt.subplots(figsize=FIGSIZE, dpi=DPI, layout='constrained')
 
-bids_hist = ax.bar(x=clean_book.loc[0]['bids'].index, height=clean_book.loc[0]['bids'],
+bids_hist = ax.bar(x=lp.book_clean.loc[0]['bids'].index, height=lp.book_clean.loc[0]['bids'],
                    color='green', label='Bids', animated=True)
-asks_hist = ax.bar(x=clean_book.loc[0]['asks'].index, height=clean_book.loc[0]['asks'],
+asks_hist = ax.bar(x=lp.book_clean.loc[0]['asks'].index, height=lp.book_clean.loc[0]['asks'],
                    color='red', label='Asks', animated=True)
 title = ax.text(x=0.5, y=0.8, s="Tick #0", ha='center', transform=ax.transAxes, animated=True)
 
 def init():
-    ax.set(ylim=(0, clean_book.max().max() * 1.1),
+    ax.set(ylim=(0, lp.book_clean.max().max() * 1.1),
        xlabel='Price ($)', ylabel='Quantity')
     ax.legend()
     return [r for r in bids_hist] + [r for r in asks_hist] + [title]
 
 def update(frame_num):
     title.set_text(f"Tick #{frame_num}")
-    for rect, quantity in zip(bids_hist.patches, clean_book.loc[frame_num]['bids']):
+    for rect, quantity in zip(bids_hist.patches, lp.book_clean.loc[frame_num]['bids']):
         rect.set_height(quantity)
-    for rect, quantity in zip(asks_hist.patches, clean_book.loc[frame_num]['asks']):
+    for rect, quantity in zip(asks_hist.patches, lp.book_clean.loc[frame_num]['asks']):
         rect.set_height(quantity)
     return [r for r in bids_hist] + [r for r in asks_hist] + [title]
 
 ani = animation.FuncAnimation(fig=fig, func=update, init_func=init,
-                              frames=len(clean_book), interval=200, blit=True)
+                              frames=len(lp.book_clean), interval=200, blit=True)
 # TODO: Switch to ImageMagickWriter or FFMpegWriter for performance
 #       https://matplotlib.org/stable/api/animation_api.html#writer-classes
 ani.save(IMG_DIR + "book.gif")
